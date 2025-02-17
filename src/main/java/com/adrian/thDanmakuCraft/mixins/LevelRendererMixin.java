0ff21a85978ecee4159788d1650db68049c55541
Asriam <1811674699a@gmail.com>
2025-02-18 04:41:06 +0800

    先上傳再說

--- src/main/java/com/adrian/thDanmakuCraft/mixins/LevelRendererMixin.java
+++ src/main/java/com/adrian/thDanmakuCraft/mixins/LevelRendererMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,11 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
 
+    @Unique
     private LevelRenderer self(){
         return (LevelRenderer) (Object) this;
     }
-
-    private Minecraft minecraft = Minecraft.getInstance();
 
     @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;entitiesForRendering()Ljava/lang/Iterable;"))
     private void renderLevel(float partialTick,
